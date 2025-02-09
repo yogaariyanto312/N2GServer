de767976d397495b8749c5b05f399d1831aa2527
yogaariyanto312 <yogaariyanto312@gmail.com>
2025-02-09 23:11:57 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const toggleButton = document.getElementById("darkModeToggle");
+const body = document.body;
+
+if (localStorage.getItem("darkMode") === "enabled") {
+    body.classList.add("dark-mode");
+    toggleButton.textContent = "☀️";
+}
+
+toggleButton.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+    if (body.classList.contains("dark-mode")) {
+        localStorage.setItem("darkMode", "enabled");
+        toggleButton.textContent = "☀️";
+    } else {
+        localStorage.setItem("darkMode", "disabled");
+        toggleButton.textContent = "🌙";
+    }
+});
